a47e4d6f2ce91bd1379e60e7ac508a4f1a89c1ff
Alphaeu <dealphaeus@gmail.com>
2024-04-17 20:13:44 +0300

    tweaks

--- sat-trak-backend/config.js
+++ sat-trak-backend/config.js
@@ -1,5 +1,27 @@
 require('dotenv').config();
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Web app's Firebase configuration
+// Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqd-3TNx_lLGMhXbuRhPuJIndTcLfJWgI",
+  authDomain: "sattrak-web-app.firebaseapp.com",
+  projectId: "sattrak-web-app",
+  storageBucket: "sattrak-web-app.appspot.com",
+  messagingSenderId: "932770170504",
+  appId: "1:932770170504:web:0560e6c58d1d102f42813d",
+  measurementId: "G-W1RYCC4JD8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 module.exports = {
     development: {
         username: process.env.DB_USER,
--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 // App.js
-import React, { useState } from 'react';
+import React, { useState, useDebugValue } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 import SatelliteInfo from './SatelliteInfo';
 import './App.css';
+import React from 'react';
 
 const App = () => {
-  const [satelliteData, setSatelliteData] = useState(null);
+  const [data, setData] = useState(null);
 
-  const searchSatellite = async (searchTerm) => {
-    try {
-      const response = await axios.get(`http://localhost:3001/api/satellite?searchTerm=${searchTerm}`);
-      setSatelliteData(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/satellite');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <h1>SatTrak - Satellite Tracking</h1>
-      <SearchBar onSearch={searchSatellite} />
-      {satelliteData && <SatelliteInfo satellite={satelliteData} />}
+      <h1>SatTrak App</h1>
+      <p>{data ? data.message : 'Loading...'}</p>
+      {/* Render more UI elements based on backend data */}
     </div>
+  );
+};
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">SatTrak Logo</div>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#tracking">Satellite Tracking</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
